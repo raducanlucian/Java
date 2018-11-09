3cf2799c8b2ed2a6a12d8131f5a4abcf09dfee84
Raducan Lucian <lucian.alexandru.raducan@gmail.com>
2018-11-09 19:14:57 +0200

    Import

---
+++ src/Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+    public static void main(String[] args) {
+        /*printam ceva
+        ttttyrtyy
+        rtyrtyrty
+         */
+
+        System.out.print("Hello world!");
+        System.out.print("Hello world!");
+    }
+}
