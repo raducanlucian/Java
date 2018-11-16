3d649b9e866197531ddbc385bc3ba0e80de1b158
Raducan Lucian <lucian.alexandru.raducan@gmail.com>
2018-11-16 16:58:39 +0200

    Import

---
+++ src/MyAge.java
@@ -0,0 +1,25 @@
+public class MyAge {
+    public static void main(String[] args) {
+    int myAge;
+    myAge = 32;
+    float weight = 60.5f;
+    double height = 356.6;
+    long distance = 10023215225522l;
+    char gender = 'm';
+    char test = 9;
+    boolean isStudent = false;
+    boolean hasJob = true;
+    int test2 = (int) weight;
+    System.out.println(test);
+    System.out.println(test2);
+
+    int i = 10;
+    int y = 10;
+    if (i == y) {
+        System.out.println("sunt egale");
+    }
+    else  {
+        System.out.println("nu sunt egale");
+}
+}
+}
